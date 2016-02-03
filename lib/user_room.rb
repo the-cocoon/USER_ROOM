adba2e376edbdd18743c22639ae1338a58ec7ea5
Ilya N. Zykin <zykin-ilya@ya.ru>
2016-02-03 22:01:09 +0300

    ~~~

--- lib/user_room.rb
+++ lib/user_room.rb
@@ -36,12 +36,19 @@ module UserRoom
     config.autoload_paths << "#{ config.root }/app/mailers/concerns/"
 
     initializer :add_user_room_engine_view_paths do
+      ActiveSupport.on_load(:active_record) do
+        _root_ = ::UserRoom::Engine.config.root
+        ::Rails.application.config.paths['db/migrate'] << "#{ _root_ }/db/migrate"
+      end
+
       ActiveSupport.on_load(:action_controller) do
         views  = "app/views/user_room"
         _root_ = ::UserRoom::Engine.config.root
         prepend_view_path("#{ _root_ }/#{ views }" ) if respond_to?(:prepend_view_path)
       end
     end
+
+    # ::HakushuIo::Application.config.paths['db/migrate'] << "#{ _views_root_ }/db/migrate"
   end
 end
 
