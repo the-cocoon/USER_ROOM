75475da4ac8fd3278a51d3c21885bd09b6b4296a
Ilya N. Zykin <zykin-ilya@ya.ru>
2016-03-17 23:02:28 +0300

    ~~~

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,3 +1,9 @@
+# OmniAuth::Strategies::Odnoklassniki.class_eval do
+#   def callback_url
+#     options.redirect_url || (full_host + script_name + callback_path)
+#   end
+# end
+
 Devise.setup do |config|
   if defined? Settings
     config.omniauth :vkontakte,
